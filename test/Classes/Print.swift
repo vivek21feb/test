7ae87427fe3d99896a4121733e1b0dbdd06f4c5e
Vivek yadav <>
2019-05-28 19:26:43 +0530

    Add file

---
+++ test/Classes/Print.swift
@@ -0,0 +1,21 @@
+
+
+import UIKit
+import Foundation
+
+
+class printStatement: NSObject {
+    class func printStar(){
+        
+        print("************")
+    }
+    
+    class func printNumbers(){
+        
+        print("1234567890")
+    }
+    class func printAlphabets(){
+        
+        print("xzcvwdfrtasvqegavge")
+    }
+}
